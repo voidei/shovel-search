79ddb582ce7448a94f19292a56a01c9aafafdc45
Dawn Walker <dawniepieuwu@gmail.com>
2023-02-10 12:42:06 +1100

    update

--- go.mod
+++ go.mod
@@ -1,6 +1,5 @@
 module github.com/voidei/shovel-search
 
-go 1.19
-
+go 1.14
 
 require github.com/valyala/fastjson v1.6.4
--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"io/ioutil"
 	"log"
+	"net/http"
 	"os"
 	"path/filepath"
+	"regexp"
 	"sort"
 	"strings"
 	"sync"
@@ -12,15 +16,23 @@ import (
 	"github.com/valyala/fastjson"
 )
 
-type match struct {
-	name, version, bin string
-}
+type (
+	match struct {
+		name, version, bin string
+	}
+	matchMap = map[string][]match
+)
 
-type matchMap = map[string][]match
+var (
+	knownBuckets       map[string]string
+	shovelSearchAPIKey string
+)
 
-// resolves the path to scoop folder
-func scoopHome() (res string) {
-	if value, ok := os.LookupEnv("SCOOP"); ok {
+// resolves the path to shovel folder
+func shovelHome() (res string) {
+	if value, ok := os.LookupEnv("SHOVEL"); ok {
+		res = value
+	} else if value, ok := os.LookupEnv("SCOOP"); ok {
 		res = value
 	} else {
 		var configHome string
@@ -51,8 +63,25 @@ func scoopHome() (res string) {
 	return
 }
 
+func shovelKnownRepos() (res map[string]string) {
+	res = make(map[string]string)
+	var parser fastjson.Parser
+
+	raw, err := os.ReadFile(shovelHome() + "\\apps\\scoop\\current\\buckets.json")
+	check(err)
+
+	result, _ := parser.ParseBytes(raw)
+	object, _ := result.Object()
+
+	object.Visit(func(k []byte, v *fastjson.Value) {
+		res[string(v.GetStringBytes())] = string(k)
+	})
+	return
+}
+
 func main() {
 	args := parseArgs()
+	knownBuckets = shovelKnownRepos()
 
 	// print posh hook and exit if requested
 	if args.hook {
@@ -61,9 +90,27 @@ func main() {
 	}
 
 	// get buckets path
-	bucketsPath := scoopHome() + "\\buckets"
+	bucketsPath := shovelHome() + "\\buckets"
 
-	// get specific buckets
+	hasResults := printResults(shovelLocalSearch(bucketsPath, args.query), false)
+	// print results and exit with status code
+	if !hasResults && shovelSearchAPIKey != "" {
+		var starsThreshold int
+		if args.popularCommunityAndUp {
+			starsThreshold = 50
+		} else {
+			starsThreshold = 0
+		}
+		hasResults = printResults(shovelSearchAPI(args.query, args.officialOnly, args.topResults, starsThreshold), true)
+	}
+
+	if !hasResults {
+		fmt.Println("No results found.")
+		os.Exit(1)
+	}
+}
+
+func shovelLocalSearch(bucketsPath string, term string) matchMap {
 	buckets, err := os.ReadDir(bucketsPath)
 	checkWith(err, "Scoop folder does not exist")
 
@@ -88,7 +135,7 @@ func main() {
 				bucketPath += "\\bucket"
 			}
 
-			res := matchingManifests(bucketPath, args.query)
+			res := matchingManifests(bucketPath, term)
 			matches.Lock()
 			matches.data[file.Name()] = res
 			matches.Unlock()
@@ -96,17 +143,109 @@ func main() {
 		}(bucket)
 	}
 	wg.Wait()
+	return matches.data
+}
 
-	// print results and exit with status code
-	if !printResults(matches.data) {
-		os.Exit(1)
+func shovelSearchAPI(term string, officialOnly bool, topResults int, starsThreshold int) matchMap {
+	var arena fastjson.Arena
+	var parser fastjson.Parser
+
+	useRegexp := strings.HasPrefix(term, "/") && strings.HasSuffix(term, "/")
+	if useRegexp {
+		term = strings.TrimPrefix(term, "/")
+		term = strings.TrimSuffix(term, "/")
 	}
+
+	body := arena.NewObject()
+	body.Set("count", arena.NewTrue())
+	if officialOnly {
+		body.Set("filter", arena.NewString("Metadata/OfficialRepositoryNumber eq 1"))
+	} else {
+		body.Set("filter", arena.NewString(""))
+	}
+	body.Set("highlight", arena.NewString(""))
+	body.Set("highlightPreTag", arena.NewString(""))
+	body.Set("highlightPostTag", arena.NewString(""))
+	body.Set("orderby", arena.NewString("search.score() desc, Metadata/OfficialRepositoryNumber desc, NameSortable asc"))
+	body.Set("search", arena.NewString(term))
+	body.Set("searchMode", arena.NewString("all"))
+	body.Set("select", arena.NewString("Name,NamePartial,NameSuffix,Version,Metadata/Repository,Metadata/OfficialRepository,Metadata/RepositoryStars"))
+	body.Set("skip", arena.NewNumberInt(0))
+	body.Set("top", arena.NewNumberInt(topResults))
+
+	request, err := http.NewRequest(
+		"POST",
+		"https://scoopsearch.search.windows.net/indexes/apps/docs/search?api-version=2020-06-30",
+		bytes.NewReader(body.MarshalTo(nil)),
+	)
+	if err != nil {
+		if strings.Contains(err.Error(), "no such host") {
+			return make(matchMap)
+		} else {
+			check(err)
+		}
+	}
+	request.Header.Set("api-key", shovelSearchAPIKey)
+	request.Header.Set("content-type", "application/json")
+
+	response, err := http.DefaultClient.Do(request)
+	check(err)
+	defer response.Body.Close()
+
+	raw, err := ioutil.ReadAll(response.Body)
+	check(err)
+
+	result, _ := parser.ParseBytes(raw)
+	values := result.GetArray("value")
+
+	var wg sync.WaitGroup
+	matches := struct {
+		sync.Mutex
+		data matchMap
+	}{}
+	matches.data = make(matchMap)
+	for _, searchResult := range values {
+		wg.Add(1)
+		go func(searchRes *fastjson.Value) {
+			defer wg.Done()
+
+			var bucket string
+
+			stars := searchRes.Get("Metadata").GetInt("RepositoryStars")
+			official := searchRes.Get("Metadata").GetBool("OfficialRepository")
+			repo := string(searchRes.Get("Metadata").GetStringBytes("Repository"))
+			if official || stars > starsThreshold {
+				if official {
+					bucket = knownBuckets[repo]
+				} else {
+					bucketSplit := strings.Split(repo, "/")
+					bucket = bucketSplit[len(bucketSplit)-2] + "_" + bucketSplit[len(bucketSplit)-1]
+				}
+				matches.Lock()
+				matches.data[bucket] = append(matches.data[bucket], match{
+					string(searchRes.GetStringBytes("Name")),
+					string(searchRes.GetStringBytes("Version")),
+					"",
+				})
+				matches.Unlock()
+			}
+		}(searchResult)
+	}
+	wg.Wait()
+	return matches.data
 }
 
 func matchingManifests(path string, term string) (res []match) {
 	term = strings.ToLower(term)
 	files, err := os.ReadDir(path)
 	check(err)
+	useRegexp := strings.HasPrefix(term, "/") && strings.HasSuffix(term, "/")
+	var re *regexp.Regexp
+	if useRegexp {
+		term = strings.TrimPrefix(term, "/")
+		term = strings.TrimSuffix(term, "/")
+		re = regexp.MustCompile("(?i)" + term)
+	}
 
 	var parser fastjson.Parser
 
@@ -127,7 +266,7 @@ func matchingManifests(path string, term string) (res []match) {
 
 		stem := name[:len(name)-5]
 
-		if strings.Contains(strings.ToLower(stem), term) {
+		if (!useRegexp && strings.Contains(strings.ToLower(stem), term)) || (useRegexp && re.MatchString(stem)) {
 			// the name matches
 			res = append(res, match{stem, version, ""})
 		} else {
@@ -167,7 +306,8 @@ func matchingManifests(path string, term string) (res []match) {
 
 			for _, bin := range bins {
 				bin = filepath.Base(bin)
-				if strings.Contains(strings.ToLower(strings.TrimSuffix(bin, filepath.Ext(bin))), term) {
+				binTrimmed := strings.ToLower(strings.TrimSuffix(bin, filepath.Ext(bin)))
+				if (!useRegexp && strings.Contains(binTrimmed, term)) || (useRegexp && re.MatchString(binTrimmed)) {
 					res = append(res, match{stem, version, bin})
 					break
 				}
@@ -183,7 +323,7 @@ func matchingManifests(path string, term string) (res []match) {
 	return
 }
 
-func printResults(data matchMap) (anyMatches bool) {
+func printResults(data matchMap, fromShovelSearch bool) (anyMatches bool) {
 	// sort by bucket names
 	entries := 0
 	sortedKeys := make([]string, 0, len(data))
@@ -191,11 +331,40 @@ func printResults(data matchMap) (anyMatches bool) {
 		entries += len(data[k])
 		sortedKeys = append(sortedKeys, k)
 	}
-	sort.Strings(sortedKeys)
+
+	if fromShovelSearch {
+		// Hoisting known buckets down to the bottom
+		sort.SliceStable(sortedKeys, func(i, j int) bool {
+			isIOfficial := !strings.Contains(sortedKeys[i], "_")
+			isJOfficial := !strings.Contains(sortedKeys[j], "_")
+			if isIOfficial && !isJOfficial {
+				return false
+			} else if !isIOfficial && isJOfficial {
+				return true
+			} else {
+				return sortedKeys[i] > sortedKeys[j]
+			}
+		})
+	} else {
+		sort.Strings(sortedKeys)
+	}
+
+	for _, k := range sortedKeys {
+		v := data[k]
+
+		if len(v) > 0 {
+			anyMatches = true
+			break
+		}
+	}
 
 	// reserve additional space assuming each variable string has length 1. Will save time on initial allocations
 	var display strings.Builder
 	display.Grow(len(sortedKeys)*12 + entries*11)
+
+	if anyMatches && fromShovelSearch {
+		fmt.Println("Results from other buckets...")
+	}
 
 	for _, k := range sortedKeys {
 		v := data[k]
@@ -204,7 +373,20 @@ func printResults(data matchMap) (anyMatches bool) {
 			anyMatches = true
 			display.WriteString("'")
 			display.WriteString(k)
-			display.WriteString("' bucket:\n")
+			display.WriteString("' bucket")
+			if fromShovelSearch {
+				if strings.Contains(k, "_") {
+					display.WriteString(" (https://github.com/")
+					display.WriteString(strings.Replace(k, "_", "/", 1))
+					display.WriteString("):\n")
+				} else {
+					display.WriteString(" (install using 'shovel install ")
+					display.WriteString(k)
+					display.WriteString("/<app>'):\n")
+				}
+			} else {
+				display.WriteString(":\n")
+			}
 			for _, m := range v {
 				display.WriteString("    ")
 				display.WriteString(m.name)
@@ -220,10 +402,6 @@ func printResults(data matchMap) (anyMatches bool) {
 			}
 			display.WriteString("\n")
 		}
-	}
-
-	if !anyMatches {
-		display.WriteString("No matches found.")
 	}
 
 	os.Stdout.WriteString(display.String())
